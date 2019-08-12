1df15fa6640bc2fb533206943392fdf328b82c53
Bakyt <mailybekov_b@iuca.kg>
2019-08-12 12:32:11 +0600

    new db added

--- KSSDA/KSSDA/settings.py
+++ KSSDA/KSSDA/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'KSSDA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alumniassociation',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
