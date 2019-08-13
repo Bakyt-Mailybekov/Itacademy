2aa2becbe041e677dab79b8b81c1a63606d56017
Bakyt <mailybekov_b@iuca.kg>
2019-08-13 13:12:28 +0600

    settings changes back

--- KSSDA/KSSDA/settings.py
+++ KSSDA/KSSDA/settings.py
@@ -78,12 +78,14 @@ WSGI_APPLICATION = 'KSSDA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'alumniassociation',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'alumniassociation',
+        #'USER': 'postgres',
+        #'PASSWORD': 'root',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
     }
 }
 
