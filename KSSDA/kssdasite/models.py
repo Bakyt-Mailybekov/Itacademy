7a0b317dad10d31e52cb6e95ca09ab134a3e593e
Bakyt <mailybekov_b@iuca.kg>
2019-08-14 17:33:15 +0600

    Model changes

--- KSSDA/kssdasite/models.py
+++ KSSDA/kssdasite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Year(models.Model):
 	YEAR_CHOICES = (
 		('2016-2017', '2016-2017'),
@@ -15,40 +16,46 @@ class Year(models.Model):
 
 
 class Language(models.Model):
-	name_language = models.CharField(max_length=50, primary_key=True, null=False)
+	name_language = models.CharField(max_length=50)
 	year_language = models.ForeignKey(Year, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name_language
 
 
+
+def upload_location(project, filename):
+	return "%s/%s" %(project.name_project, filename)
+
 class Project(models.Model):
-	name_project = models.CharField(max_length=255, primary_key=True, null=False)
+	name_project = models.CharField(max_length=255)
 	description_project = models.TextField(blank=True, null=True)
-	picture_project = models.ImageField(upload_to='', max_length=50, blank=True, null=True)
-	year_project = models.ForeignKey(Year, default="", on_delete=models.CASCADE)
-	language_project = models.ForeignKey(Language, default="", on_delete=models.CASCADE)
+	picture_project = models.ImageField(upload_to=upload_location, max_length=50, blank=True, null=True)
+	year_project = models.ForeignKey(Year, default='', on_delete=models.CASCADE, blank=True)
+	#language_project = models.ForeignKey(Language, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name_project
 
 
 
+def upload_location(alumni, filename):
+	return "%s/%s" %(alumni.last_name_alumni, filename)
+
 class Alumni(models.Model):
 	class Meta:
 		verbose_name_plural = 'Alumni'
-	first_name_alumni = models.CharField(max_length=32, primary_key=True, null=False)
+
+	first_name_alumni = models.CharField(max_length=32)
 	last_name_alumni = models.CharField(max_length=32)
-	picture_alumni = models.ImageField(upload_to='', max_length=255, blank=True, null=True)
+	picture_alumni = models.ImageField(upload_to=upload_location, max_length=255, blank=True, null=True)
 	description_alumni = models.TextField(blank=True, null=True)
 	email_alumni = models.EmailField(max_length=50, blank=True, null=True)
 	phone_number_alumni = models.CharField(max_length=32, blank=True, null=True)
 	github_link_alumni = models.URLField(blank=True, null=True)
-	cv_alumni = models.FileField(upload_to='docs/', default="")
+	cv_alumni = models.FileField(upload_to=upload_location, default="")
 	project_alumni = models.ForeignKey(Project, default="", on_delete=models.CASCADE)
 
 
 	def __str__(self):
 		return self.first_name_alumni + " " + self.last_name_alumni
-
-
