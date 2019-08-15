5fb3a8d3edb2ca0e1cacfd037e825f2617a1b4c3
Bakyt <mailybekov_b@iuca.kg>
2019-08-15 14:18:31 +0600

    integration new index.html file

--- KSSDA/kssdasite/views.py
+++ KSSDA/kssdasite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 
 def home(request):
-    return render(request, 'kssdasite/it-academy.html')
+    return render(request, 'kssdasite/index.html')
 
 
 
