885e2b03fa0165d80ad66e8cb2ac84a823bb5ed4
Bakyt <mailybekov_b@iuca.kg>
2019-08-15 16:55:01 +0600

    norm

--- KSSDA/kssdasite/admin.py
+++ KSSDA/kssdasite/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-#admin.site.register(Year)
-#admin.site.register(Language)
-#admin.site.register(Project)
-#admin.site.register(Alumni)
+admin.site.register(Year)
+admin.site.register(Language)
+admin.site.register(Project)
+admin.site.register(Alumni)
 # Register your models here.
